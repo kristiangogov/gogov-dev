f3165ace7ccbd6fc384c9c8cd9d1bd909bfa9147
b1urrr <kgogov@coca-cola.com>
2024-12-31 08:41:55 +0200

    Implement Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Counter from "./features/counter/counter"
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Counter/>
       <div>
       <h1>gogov.dev</h1>
         <p>coming soon...</p>
---
+++ src/features/counter/counter.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from './counterSlice'
+
+function Counter() {
+  const count = useSelector(state => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <p>{count}</p>
+        <button
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
